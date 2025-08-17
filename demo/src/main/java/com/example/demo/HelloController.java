a6c5013fda93fa6598a8261bf9254a4b135ca248
carlosfhz13 <carlosfhz13@gmail.com>
2025-08-17 14:01:50 +0200

    Set up Spring

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "World") String name) {
+        return "Hello "+name+"!";
+    }
+}
---
+++ demo/src/main/java/com/example/demo/OaimsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaimsApplication.class, args);
+	}
+
+}
